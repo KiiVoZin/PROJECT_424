1d9fca8b6fe9a52891301f9ce6350e78c7b1885f
Utku <71753904+welsdarte@users.noreply.github.com>
2022-06-20 12:35:44 +0300

    basic enemy model and movement

--- PROJECT_424/Assets/Scripts/Enemy.cs
+++ PROJECT_424/Assets/Scripts/Enemy.cs
@@ -7,16 +7,55 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] GameObject Player;
 
-    public float moveSpeed=1.5f;
+    public float moveSpeed           = 1.5f;
+    public float turnAroundSpeed     = 2.0f;
+    public float dashSpeed           = 5.0f;
+    public float dashTime            = 2.0f;
+    public float dashTimeCurrent     = 2.0f;
+    public float dashCooldown        = 5.0f;
+    public float dashCooldownCurrent = 5.0f;
+    public bool  dashReady           = false;
+    public Vector3 dashTarget;
     void Start()
     {
     }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.position=Vector3.MoveTowards(transform.position,Player.transform.position,moveSpeed*Time.deltaTime);
-        //transform.up=Player.transform.position-transform.position;
+    {   
+        
+        //and distance is greater than some d
+        if(dashReady ){
+            dashTimeCurrent -= Time.deltaTime;
+            dashTarget = Player.transform.position;
+            if(dashTimeCurrent < 0){
+                dashReady = false;
+                dashTimeCurrent = dashTime;
+            }else{
+                //dash through to player's last position
+                transform.position=Vector3.MoveTowards(transform.position,dashTarget, dashSpeed*Time.deltaTime);
+
+            }
+        }else{
+            //move towards to player normally
+            transform.position=Vector3.MoveTowards(transform.position,Player.transform.position + new Vector3(0.0f,1.5f,0.0f),(moveSpeed*dashCooldownCurrent/dashCooldown)*Time.deltaTime);
+
+            dashCooldownCurrent -= Time.deltaTime;
+            if(dashCooldownCurrent < 0){
+                dashReady = true;
+                dashCooldownCurrent = dashCooldown;
+            }
+        }
+        
+
+        
+        
+
+        //slowly look at player
+        Vector3 relativePos = Player.transform.position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Lerp( transform.rotation, toRotation, turnAroundSpeed * Time.deltaTime );
+        
 
         
         
