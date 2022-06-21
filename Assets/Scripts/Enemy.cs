bccca4103c89b42845af6670dbc8a4022d62bded
Utku <71753904+welsdarte@users.noreply.github.com>
2022-06-22 00:15:57 +0300

    Skull movement improved

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] GameObject Player;
     Collider m_ObjectCollider;
-    
+    Rigidbody m_Rigidbody;
    
     public float moveSpeed           = 2.2f;
     public float turnAroundSpeed     = 3.3f;
@@ -21,7 +21,8 @@ public class Enemy : MonoBehaviour
     void Start()
     {
 
-        
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
         //Fetch the GameObject's Collider (make sure they have a Collider component)
         m_ObjectCollider = GetComponent<Collider>();
         //Here the GameObject's Collider is not a trigger
@@ -31,23 +32,25 @@ public class Enemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {   
-        //and distance is greater than some d
+        
         if(dashReady ){
             dashTimeCurrent -= Time.deltaTime;
             
-            if(dashTimeCurrent <= 0){
+            if(dashTimeCurrent < 0){
                 dashReady = false;
                 dashTimeCurrent = dashTime;
             }else{
                 //dash through to player's last position
-                transform.position = Vector3.MoveTowards(transform.position, dashTarget, dashSpeed * Time.deltaTime);
+                //transform.position = Vector3.MoveTowards(transform.position, dashTarget, dashSpeed * Time.deltaTime);
+                m_Rigidbody.AddForce(transform.forward * dashSpeed * Time.deltaTime);
                 //transform.Translate(Vector3.forward * dashSpeed * Time.deltaTime);
             }
         }else{
             //move towards to player normally
-            transform.position=Vector3.MoveTowards(transform.position,Player.transform.position , (moveSpeed) * Time.deltaTime);
+            //transform.position=Vector3.MoveTowards(transform.position,Player.transform.position , (moveSpeed) * Time.deltaTime);
+            m_Rigidbody.AddForce(transform.forward * moveSpeed/(dashCooldownCurrent+1.0f) * Time.deltaTime);
 
             dashCooldownCurrent -= Time.deltaTime;
             if(dashCooldownCurrent <= 0){
@@ -60,12 +63,13 @@ public class Enemy : MonoBehaviour
 
         //slowly look at player
         if(!dashReady){
-            
+            //fly above to observe
+            m_Rigidbody.AddForce(transform.up * Random.Range(12.0f / dashCooldown, 240.0f / dashCooldown) * Time.deltaTime);
+
             Vector3 relativePos = Player.transform.position - transform.position;
             Quaternion toRotation = Quaternion.LookRotation(relativePos);
             transform.rotation = Quaternion.Lerp( transform.rotation, toRotation, turnAroundSpeed * Time.deltaTime );
         }
         //transform.LookAt(Player.transform);
-
     }
 }
