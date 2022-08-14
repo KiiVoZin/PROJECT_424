64a9fd4f97f37592de48238441509172bc847e11
KiiVoZin <58093039+KiiVoZin@users.noreply.github.com>
2022-08-14 16:43:20 +0300

    Pause

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,7 +22,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGame();
         }
